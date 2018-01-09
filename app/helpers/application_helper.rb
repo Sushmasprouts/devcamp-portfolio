73d6df5040d7c52d7cf43b7f83564f1b9344958b
sushma <sushma@sproutssolutions.com>
2018-01-09 12:30:02 +0000

    implemented helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def source_helper(layout_name)
+    if session[:source] 
+        gretting= "Thanks for visiting me from  #{ session[:source]} and you are on the #{layout_name} layout "
+        content_tag(:p,gretting,class:"source-gretting")
+    end 
+  end
 end
